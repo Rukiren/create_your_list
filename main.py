c72faaa5bb50219b9115bd9c6cc0ddb8ce074b6e
Ruki <75606879+Rukiren@users.noreply.github.com>
2023-03-08 23:31:11 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ with open('info.txt', 'r') as f:
     var1 = f.read()
 
 
-
-
 var3 = '1000'
+
 url = 'https://github.com/Rukiren/create_your_list'
-prompt = f'請閱讀以下內容[{var1}]，將其中的內容整理成一份具有完整敘述的履歷，履歷需條列式並詳述，方便產生下一份報告，並在結尾加上報告內容是透過此連結專案產生: {url}'
+prompt = f'跟據[info]，將[info]的內容整理成一份具有完整敘述的履歷，整理出大標題與小標題與內容詳述，履歷需條列式並詳述，避免使用重複的連接詞，最後在最後一行獨立加上報告內容的產生是透過此連結專案產生: {url}\n [info] = {var1}'
 
 response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=2000)
 
